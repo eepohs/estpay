e4df476d8a633532b51b7dc452e1cfe07dceb435
Sven Varkel <sven.varkel@eepohs.com>
2012-02-29 13:48:07 +0200

    Bank logos

--- htdocs/app/code/local/Eepohs/EEPay/Block/Seb.php
+++ htdocs/app/code/local/Eepohs/EEPay/Block/Seb.php
@@ -1,8 +1,9 @@
 <?php
 
-class Eepohs_EEPay_Block_Seb extends Eepohs_EEPay_Block_IPizza {
+class Eepohs_EEPay_Block_Seb extends Eepohs_EEPay_Block_IPizza
+{
 
     protected $_code = 'eepohs_seb';
     protected $_gateway = 'seb';
-        
+
 }
--- htdocs/app/code/local/Eepohs/EEPay/Block/Swedbank.php
+++ htdocs/app/code/local/Eepohs/EEPay/Block/Swedbank.php
@@ -1,8 +1,9 @@
 <?php
 
-class Eepohs_EEPay_Block_Swedbank extends Eepohs_EEPay_Block_IPizza {
+class Eepohs_EEPay_Block_Swedbank extends Eepohs_EEPay_Block_IPizza
+{
 
     protected $_code = 'eepohs_swedbank';
     protected $_gateway = 'swedbank';
-        
+
 }
