0b97026f270c425a380f91b278ebad0533b308ba
Sven Varkel <sven.varkel@eepohs.com>
2012-04-16 23:06:48 +0300

    Adminhtml changes

---
+++ htdocs/app/code/community/Eepohs/EEPay/Block/Initjs.php
@@ -0,0 +1,30 @@
+<?php
+
+class Eepohs_EEPay_Block_Initjs extends Mage_Adminhtml_Block_Template
+{
+
+    /**
+     * Include JS in the head if section is Eepohs/EEPay
+     */
+    protected function _prepareLayout()
+    {
+        $section = $this->getAction()->getRequest()->getParam('section', false);
+        if ($section == 'payment') {
+            $this->getLayout()
+                    ->getBlock('head')
+                    ->addJs('eepohs/eepay.js');
+        }
+        parent::_prepareLayout();
+    }
+
+    protected function _toHtml()
+    {
+        $section = $this->getAction()->getRequest()->getParam('section', false);
+        if ($section == 'payment') {
+            return parent::_toHtml();
+        } else {
+            return '';
+        }
+    }
+
+}
