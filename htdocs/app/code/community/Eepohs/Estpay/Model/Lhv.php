f67dd2aa1f807b23ff095069c3f13d2e13fe80af
Jaanus Vapper <jaanus.vapper@eepohs.com>
2012-09-20 12:35:03 +0300

    LHV payment method

---
+++ htdocs/app/code/community/Eepohs/Estpay/Model/Lhv.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @package    Eepohs
+ * @subpackage Estpay
+ */
+
+/**
+ * Estpay Model for Swedbank
+ *
+ * PLEASE READ THIS SOFTWARE LICENSE AGREEMENT ("LICENSE") CAREFULLY
+ * BEFORE USING THE SOFTWARE. BY USING THE SOFTWARE, YOU ARE AGREEING
+ * TO BE BOUND BY THE TERMS OF THIS LICENSE.
+ * IF YOU DO NOT AGREE TO THE TERMS OF THIS LICENSE, DO NOT USE THE SOFTWARE.
+ *
+ * Full text of this license is available @license
+ *
+ * @license http://www.eepohs.com/eepohs-commercial-software-license/
+ * @version $version$
+ * @author Eepohs OÜ
+ * @copyright $year$ Eepohs OÜ http://www.eepohs.com/
+ *
+ * @package    Eepohs
+ * @subpackage Estpay
+ * @category   Payment methods
+ */
+class Eepohs_Estpay_Model_Lhv extends Eepohs_Estpay_Model_IPizza
+{
+
+    protected $_code = 'eepohs_lhv';
+    protected $_formBlockType = 'estpay/lhv';
+    protected $_gateway = 'lhv';
+
+}
---
+++ htdocs/app/code/community/Eepohs/Estpay/controllers/LhvController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @package    Eepohs
+ * @subpackage Estpay
+ */
+
+/**
+ * Controller for Swedbank of Estpay payment method
+ *
+ * PLEASE READ THIS SOFTWARE LICENSE AGREEMENT ("LICENSE") CAREFULLY
+ * BEFORE USING THE SOFTWARE. BY USING THE SOFTWARE, YOU ARE AGREEING
+ * TO BE BOUND BY THE TERMS OF THIS LICENSE.
+ * IF YOU DO NOT AGREE TO THE TERMS OF THIS LICENSE, DO NOT USE THE SOFTWARE.
+ *
+ * Full text of this license is available @license
+ *
+ * @license http://www.eepohs.com/eepohs-commercial-software-license/
+ * @version $version$
+ * @author Eepohs OÜ
+ * @copyright $year$ Eepohs OÜ http://www.eepohs.com/
+ *
+ * @package    Eepohs
+ * @subpackage Estpay
+ * @category   Payment methods
+ */
+class Eepohs_Estpay_LhvController extends Eepohs_Estpay_Controller_Abstract
+{
+
+    protected $_model = 'estpay/lhv';
+    protected $_code = 'eepohs_lhv';
+
+}
