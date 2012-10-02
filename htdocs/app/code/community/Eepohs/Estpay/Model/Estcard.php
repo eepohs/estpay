6040cf1914b0c2d1255dc7363b48a61ce3dd98b5
Sven Varkel <sven.varkel@eepohs.com>
2012-10-02 16:08:21 +0300

    Added BDS license text to all files

--- htdocs/app/code/community/Eepohs/Estpay/Model/Estcard.php
+++ htdocs/app/code/community/Eepohs/Estpay/Model/Estcard.php
@@ -13,9 +13,28 @@
  * TO BE BOUND BY THE TERMS OF THIS LICENSE.
  * IF YOU DO NOT AGREE TO THE TERMS OF THIS LICENSE, DO NOT USE THE SOFTWARE.
  *
- * Full text of this license is available @license
+ * Copyright (c) $year$, Eepohs OÜ
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @license http://www.eepohs.com/eepohs-commercial-software-license/
+ * @license http://opensource.org/licenses/bsd-license.php
  * @version $version$
  * @author Eepohs OÜ
  * @copyright $year$ Eepohs OÜ http://www.eepohs.com/
@@ -40,7 +59,7 @@ class Eepohs_Estpay_Model_Estcard extends Eepohs_Estpay_Model_Abstract
             Mage::log(sprintf('%s (%s): Wrong merchant ID used for return: %s vs %s', __METHOD__, __LINE__, $params['id'], $merchantId));
             return false;
         }
-        
+
         $data =
             sprintf("%03s", $params['ver'])
             . sprintf("%-10s", $params['id'])
--- htdocs/app/code/community/Eepohs/Estpay/Model/IPizza.php
+++ htdocs/app/code/community/Eepohs/Estpay/Model/IPizza.php
@@ -13,9 +13,28 @@
  * TO BE BOUND BY THE TERMS OF THIS LICENSE.
  * IF YOU DO NOT AGREE TO THE TERMS OF THIS LICENSE, DO NOT USE THE SOFTWARE.
  *
- * Full text of this license is available @license
+ * Copyright (c) $year$, Eepohs OÜ
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
  *
- * @license http://www.eepohs.com/eepohs-commercial-software-license/
+ * @license http://opensource.org/licenses/bsd-license.php
  * @version $version$
  * @author Eepohs OÜ
  * @copyright $year$ Eepohs OÜ http://www.eepohs.com/
@@ -35,7 +54,7 @@ class Eepohs_Estpay_Model_IPizza extends Eepohs_Estpay_Model_Abstract
             return false;
         }
 
-        
+
         $vkSndId = Mage::getStoreConfig('payment/' . $this->_code . '/vk_snd_id');
 
         if ( !isset($params['VK_SND_ID']) || $params['VK_SND_ID'] != $vkSndId ) {
