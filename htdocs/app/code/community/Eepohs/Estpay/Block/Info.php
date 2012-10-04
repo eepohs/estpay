0096ad796be96173a187e76cfc01242608f4c6bd
Sven Varkel <sven.varkel@eepohs.com>
2012-10-04 11:21:53 +0300

    Added info block for logo images in footer or elsewhere

---
+++ htdocs/app/code/community/Eepohs/Estpay/Block/Info.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * Info.php
+ *
+ * PHP version 5
+ *
+ * @category   Magento
+ * @package    Eepohs
+ * @subpackage Estpay
+ * @author     Eepohs OÜ <info@eepohs.com>
+ * @license    http://opensource.org/licenses/bsd-license.php BSDL
+ * @link       http://eepohs.com/
+ */
+
+/**
+ * Information block that returns list of enabled banks
+ *
+ * PLEASE READ THIS SOFTWARE LICENSE AGREEMENT ("LICENSE") CAREFULLY
+ * BEFORE USING THE SOFTWARE. BY USING THE SOFTWARE, YOU ARE AGREEING
+ * TO BE BOUND BY THE TERMS OF THIS LICENSE.
+ * IF YOU DO NOT AGREE TO THE TERMS OF THIS LICENSE, DO NOT USE THE SOFTWARE.
+ *
+ * Copyright (c) 2012, Eepohs OÜ
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   Community
+ * @package    Eepohs
+ * @subpackage Estpay
+ * @author     Eepohs OÜ <info@eepohs.com>
+ * @copyright  2012 Eepohs OÜ
+ * @license    http://opensource.org/licenses/bsd-license.php BSDL
+ * @version    Release $version$
+ * @link       http://eepohs.com/
+ *
+ */
+class Eepohs_Estpay_Block_Info
+    extends Mage_Core_Block_Template
+{
+
+    /**
+     * Returns array of enabled Estpay
+     * gateways
+     *
+     * @return array
+     */
+    public function getEnabledGateways()
+    {
+        $paymentMethods = Mage::getSingleton('payment/config')->getActiveMethods();
+        $methods = array();
+        foreach ( $paymentMethods as $paymentCode => $paymentModel ) {
+            if ( $paymentModel instanceof Eepohs_Estpay_Model_Abstract ) {
+                $paymentTitle = Mage::getStoreConfig('payment/' . $paymentCode . '/title');
+                $formBlockType = $paymentModel->getFormBlockType();
+                $formBlockInstance = Mage::getBlockSingleton($formBlockType);
+                $code = $paymentCode;
+                $methods[] = array(
+                'title' => $paymentTitle,
+                'code' => $paymentCode,
+                'logo' => $formBlockInstance->getMethodLogoUrl()
+                 );
+            }
+        }
+        return $methods;
+    }
+
+}
